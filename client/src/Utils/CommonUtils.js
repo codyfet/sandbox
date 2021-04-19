7e99551c10a1f99921c73dcfed5c19411cc09dfd
codyfet <codyfet@mail.ru>
2021-04-19 23:53:27 +0300

    Добавил проверку на email'

--- client/src/Utils/CommonUtils.js
+++ client/src/Utils/CommonUtils.js
@@ -6,3 +6,13 @@
 export function getSolvedCount(appState) {
     return appState.session.tasks.filter((item) => item.solved).length;
 }
+
+/**
+ * Проверяет валиден ли введённый email.
+ *
+ * @param {string} email Email.
+ */
+export function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
